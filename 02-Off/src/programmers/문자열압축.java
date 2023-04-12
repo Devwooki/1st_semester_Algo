f638dfb68df880f1409fd4f3c538f8724c4c0729
gmzuddl <>
2023-04-12 21:47:58 +0900

    [Update] : 문자열압축

--- 02-Off/src/programmers/문자열압축.java
+++ 02-Off/src/programmers/문자열압축.java
@@ -6,6 +6,7 @@ import java.io.*;
 
 public class 문자열압축 {
     static String[] s = {
+            "a",
             "aabbaccc",
             "ababcdcdababcdcd",
             "abcabcdede",
@@ -14,15 +15,60 @@ public class 문자열압축 {
     };
     static int Solution(String str){
         int answer = Integer.MAX_VALUE;
-        for(int i = 0 ; i <= str.length()/2 ; ++i){
-            answer = Math.min(answer, zip(i,str));
+        //완전 탐색으로 진행
+        //
+        for(int i = 1 ; i <= str.length()/2 ; ++i){
+            //최솟값을 지정한다.
+            answer = Math.min(answer, zip(str, i));
         }
+
         return answer;
     }
 
-    private static int zip(int len, String str) {
+    /*
+    선정할 문자열 -> 입력문자열.subString(시작위치, 시작위치+len)
+    현재 문자열 == 이전 문자열 -{
+        cnt 증가
+    } 현재 문자열 != 이전 문자열 {
+        sb.append(cnt).append(prev), cnt = 0;
+    }
+    prev = now;
 
 
+    return sb.length();
+     */
+    private static int zip(String str, int len) {
+        int textLength = str.length();;
+        StringBuilder sb = new StringBuilder();
+        if(textLength == 1) return 1;
+        String prev = "";
+        int cnt = 1;
+        for(int i = 0 ; i < textLength ; i += len){
+            String now = "";
+            if(i + len < textLength) now = str.substring(i, len + i);
+            else now = str.substring(i, textLength);
+
+            if(now.equals(prev)){
+                cnt++;
+            }else{
+                if(prev.equals("")) {
+                    prev = now;
+                    continue;
+                }
+
+                if(cnt == 1) sb.append(prev);
+                else sb.append(cnt).append(prev);
+
+                cnt = 1;
+            }
+
+            prev = now;
+        }
+
+        //마지막에 남은 것을 붙인가
+        if(cnt == 1) sb.append(prev);
+        else sb.append(cnt).append(prev);
+        return sb.toString().length();
     }
 
 
