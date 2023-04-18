205eb67ebb8e346e164148fc4de392cd819ac435
gmzuddl <70623636+gmzuddl@users.noreply.github.com>
2023-04-18 18:00:13 +0900

    [Update] : 스터디 문제 풀이 3/4 완료

--- 02-Off/src/study/BOJ16234.java
+++ 02-Off/src/study/BOJ16234.java
@@ -1,44 +1,30 @@
 package study;
+
 import java.io.*;
 import java.util.*;
 
-//2^괄호 쌍 수 - 1, 총 경우의 수
+/*
+- 국경을 공유하는 두나라 인구차이가 L이상 R이하면 국경선 하루 연다
+- 국경선 열리면 인구이동 가능 -> 그날 하루 연합
+- 연합을 이루고 있는 각 칸의 인구수
+  (연합의 인구수) / 연합을 이루고 있는 칸의 개수
+- 소수 버림
+*/
 public class BOJ16234 {
-    static int[] numbers;
-    static List<int []> list;
-    static char[] str;
-    public static void main(String[] args) throws IOException{
+    static int N, L, R;
+    static int guideLine;
+    static int[][] dir = {{1,0},{0,1},{-1,0},{0,-1}};
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
-        str = br.readLine().toCharArray();
+	    StringTokenizer st = new StringTokenizer(br.readLine());
+	    StringBuilder sb = new StringBuilder();
+
+        N = Integer.parseInt(st.nextToken());
+        L = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+        guideLine = L-R;
 
 
-        list = new ArrayList<>();
-        Stack<Integer> stack = new Stack<>();
-        for(char c : str){
-            if(c == '(') stack.add(i);
-            if(c == ')'){
-                int start = stack.pop();
-                list.add(new int[]{start, i});
-            }
-        }
-
-        int size = list.size();
-        numbers = new int[size];
-
-        //부분집합인데 모두가 선택되는 경우는 제외한다
-        subSetBit(size);
-    }
-
-    private static void subSetBit(int size){
-        for(int i = 1 ; i < (1 << size) ; ++i){
-            for(int j = 0 ; j < size; ++j){
-                if((i & (1 << j)) != 0 ) {
-                    //부분
-                }
-            }
-            System.out.println();
-        }
     }
 }
+
--- 02-Off/src/study/BOJ2800.java
+++ 02-Off/src/study/BOJ2800.java
@@ -5,9 +5,59 @@ import java.util.*;
 
 //유효한 괄호만 남긴다
 public class BOJ2800 {
+    static int[] numbers;
+    static List<int []> list;
+    static char[] str;
+    static boolean[] brackets;
+    static TreeSet<String> set = new TreeSet<>();
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
+        str = br.readLine().toCharArray();
+
+        list = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
+        for(int i = 0 ; i < str.length ; ++i){
+            if(str[i] == '(') stack.add(i);
+            if(str[i] == ')'){
+                int start = stack.pop();
+                list.add(new int[]{start, i});
+            }
+        }
+
+        int size = list.size();
+        numbers = new int[size];
+
+        //부분집합인데 모두가 선택되는 경우는 제외한다
+        subSetBit(size);
+
+        for(String answer : set){
+            sb.append(answer+"\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    private static void subSetBit(int size){
+        for(int i = 1 ; i < (1 << size) ; ++i){
+            brackets = new boolean[str.length];
+            for(int j = 0 ; j < size; ++j){
+                if((i & (1 << j)) != 0 ) {
+                    brackets[list.get(j)[0]] = true;
+                    brackets[list.get(j)[1]] = true;
+                    makeFormula();
+                }
+            }
+        }
+    }
+
+    private static void makeFormula(){
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0 ; i < str.length ; ++i){
+            if(brackets[i])continue;
+            sb.append(str[i]);
+        }
+        set.add(sb.toString());
     }
 }
