58c70b0efe280a11980cb991abb6f6ba0bbb2616
gmzuddl <70623636+gmzuddl@users.noreply.github.com>
2023-04-20 18:00:46 +0900

    dtd

--- 02-Off/src/study/BOJ2668.java
+++ 02-Off/src/study/BOJ2668.java
@@ -3,7 +3,7 @@ import java.io.*;
 import java.util.*;
 
 public class BOJ2668 {
-    static ArrayList<Integer>[] graph;
+    static int[] graph;
     static boolean[] visited;
     static int N, max = Integer.MIN_VALUE;
     static PriorityQueue<Integer> pq;
@@ -15,41 +15,35 @@ public class BOJ2668 {
         N = Integer.parseInt(br.readLine());
 
         //인접행렬 초기화
-        graph = new ArrayList[N+1];
-        for(int i = 0 ; i <= N ; ++i){
-            graph[i] = new ArrayList<>();
-        }
-
+        graph = new int[N+1];
 
         for(int i = 1 ; i <= N ; ++i){
-            graph[i].add(Integer.parseInt(br.readLine()));
+            graph[i] =Integer.parseInt(br.readLine());
         }
 
         //bfs를 돌면서 가장 큰 사이클을 발견
         for(int i = 1 ; i <= N ; ++i){
             visited = new boolean[N+1];
-            int cycle= dfs(i, 0);
-
-            if(cycle != -1){
-
-            }
+            dfs1(i, 0);
+            dfs2(i,i);
+        }
+    }
+    //가설 1.
+    //사이클이 가장 큰 것을 찾고, 자기 자신을 가리키는 것 cnt
+    private static void dfs1(int now, int cnt) {
+        if(!visited[graph[now]]) {
+            visited[now] = true;
+            dfs1(graph[now], cnt+1);
         }
     }
 
-    private static int dfs(int now, int cnt) {
-
-        visited[now] = true;
-        for(int next : graph[now]){
-            if(visited[next]) return cnt;
-            else{
-                dfs(next, cnt+1);
-            }
+    //가설 2.
+    //사이클의 갯수 파악 - 사이클이 발생하는 번호를 pq에 넣기
+    private static void dfs2(int now, int cnt) {
+        if(!visited[graph[now]]) {
+            visited[now] = true;
+            dfs2(graph[now], cnt+1);
         }
     }
 
 }
-
-/*
-가설 1.
-사이클이 가장 큰 것을 찾고, 자기 자신을 가리키는 것 cnt
-*/
