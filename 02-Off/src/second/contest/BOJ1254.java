0d8ec0f833bc677b0c24f29cacd8bb497fa0e344
devwooki <dldnr789@koreatech.ac.kr>
2024-02-01 23:06:53 +0900

    백준 1254

--- 02-Off/src/second/contest/BOJ1254.java
+++ 02-Off/src/second/contest/BOJ1254.java
@@ -6,10 +6,33 @@ import java.util.*;
 public class BOJ1254 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
 
-        String originStr = br.readLine();
-        String reverseStr = sb.append(originStr).reverse().toString();
+        String str = br.readLine();
+        int strLen = str.length();
+
+        int appendCharCnt = 0;
+
+        //문자를 앞에서 부터 한칸씩 자른다.
+        //자른 문자열이 펠린드롬이 되면, 그
+        for(int i = 0 ;  i < strLen ; ++i){
+            if(isPalind(str.substring(i))){
+                break;
+            }
+            appendCharCnt += 1;
+        }
+
+        System.out.println(strLen + appendCharCnt);
+    }
+
+    public static boolean isPalind(String subStr){
+        int start = 0;
+        int end = subStr.length() - 1;
+
+        while(start <= end){
+            if(subStr.charAt(start) != subStr.charAt(end)) return false;
+                start +=1;
+                end -=1;
+        }
+        return true;
     }
 }
