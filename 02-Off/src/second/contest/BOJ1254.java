bb451e3dfb1cbd5354a73384ffabf7a6ee6d3ab4
이현욱 <dldnr789@gmail.com>
2024-02-01 22:18:19 +0900

    백준 1254

---
+++ 02-Off/src/second/contest/BOJ1254.java
@@ -0,0 +1,15 @@
+package second.contest;
+import java.io.*;
+import java.util.*;
+
+
+public class BOJ1254 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        String originStr = br.readLine();
+        String reverseStr = sb.append(originStr).reverse().toString();
+    }
+}
